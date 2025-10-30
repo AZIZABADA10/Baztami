967aabe69fab9039d0336881a903721a732d87be
AZIZABADA10 <abadaaziz288@gmail.com>
2025-10-30 23:55:44 +0000

    add: affichage du carte de transaction moderne via tailwind css

--- js/main.js
+++ js/main.js
@@ -43,6 +43,39 @@ formAjouter.addEventListener('submit', (event) => {
     TableauTransactions.push(transaction);
 
     i++;
+    afficherTransactions();
     formAjouter.reset();
     formContainer.classList.add('hidden');
 });
+
+
+/** Fonction pour afficher les transactions **/
+function afficherTransactions() {
+
+    listTransactions.innerHTML = '';
+    // Pour chaque transaction, on crée une carte
+    TableauTransactions.forEach((t) => {
+        const card = document.createElement('div');
+
+        card.innerHTML = `
+            <div class="${t.type === 'revenu' ? ' bg-green-500 hover:bg-green-900' : ' bg-red-500 hover:bg-red-900'} m-4 rounded-xl py-2 px-4 border border-gray-200">
+                <p class="font-bold text-white text-lg mb-1">${t.description}</p>
+                <p class="font-semibold text-white text-xl mb-2">${t.type === 'revenu' ? '+' : '-'} ${t.montant} DH </p>
+                    <div class="flex items-center gap-2 text-gray-200">
+                        <i class='bx bx-calendar-alt text-lg'></i>
+                        <span class="text-sm">${t.date}</span>
+                        <div class="flex gap-3 ml-4">
+                            <button>
+                                <i class='bx bx-edit text-white text-lg'></i>
+                            </button>
+                            <button >
+                                <i class='bx bx-trash text-white text-lg'></i>
+                            </button>
+                        </div>
+                    </div>
+            </div>
+        `;
+
+        listTransactions.appendChild(card);
+    });
+}
